d35e56bcf9bb920076baab450c463559d02690d5
kodarit11 <taavi.makela@gmail.com>
2021-02-25 18:13:48 +0200

    liikkuminen ja painovoima ja hyppy tehty

---
+++ 2021KevatTo17153DFPS_React/2021KevatTo1715_unityFPS/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    private Vector3 velocity = Vector3.zero;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical"); 
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight* -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
