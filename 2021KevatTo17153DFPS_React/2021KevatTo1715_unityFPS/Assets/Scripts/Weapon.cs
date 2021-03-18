7adc076dc72e5416b7c225bf07a9fa0a1e3fb441
kodarit11 <taavi.makela@gmail.com>
2021-03-18 18:27:21 +0200

    heitto aloitettu

---
+++ 2021KevatTo17153DFPS_React/2021KevatTo1715_unityFPS/Assets/Scripts/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public float throwForce = 10f;
+    public GameObject throwObjPrefab;
+    private Camera FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Throw();
+        }
+    }
+
+    private void Throw()
+    {
+     GameObject obj = Instantiate(throwObjPrefab,transform.position,Quaternion.identity);
+     obj.GetComponent<Rigidbody>().AddForce(FPSCamera.transform.forward * throwForce);
+    }
+}
