e4937baf02554d6f2711d13afd1297946aa838a5
kodarit11 <taavi.makela@gmail.com>
2021-04-29 18:14:51 +0300

    journal on valmis. Player Interact seuraavaksi

--- 2021KevatTo17153DFPS_React/2021KevatTo1715_unityFPS/Assets/Scripts/Journal.cs
+++ 2021KevatTo17153DFPS_React/2021KevatTo1715_unityFPS/Assets/Scripts/Journal.cs
@@ -7,7 +7,10 @@ using TMPro;
 public class Journal : MonoBehaviour
 {
     public static Journal Instance;
-
+    public TMP_Text textBox;
+    public GameObject textBoxImage;
+    public float textFadeOutDuration = 1;
+    private float countdown;
     void Awake()
     {
         if(Instance != null)
@@ -18,16 +21,36 @@ public class Journal : MonoBehaviour
         {
             Instance = this;
         }
+
+        DisableTextBox();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(textBoxImage.activeSelf)
+        {
+            countdown -= Time.deltaTime;
+
+            if(countdown <= 0)
+            {
+                DisableTextBox();
+            }
+        }
     }
 
     public void Log(string text)
     {
-        print(text);
+        //print(text);
+        textBoxImage.SetActive(true);
+        textBox.text = text;
+        //käynnistetään laskuri tekstin piilottamiseksi
+        countdown = textFadeOutDuration;
+    }
+
+    private void DisableTextBox()
+    {
+        textBox.text = "";
+        textBoxImage.SetActive(false);
     }
 }
---
+++ 2021KevatTo17153DFPS_React/2021KevatTo1715_unityFPS/Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+    public float range = 2;
+    Camera FPSCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        FPSCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
